968b5382713e031a9fd1b6c29001e905616baa2b
VictorSandbox <victor.w.cairo>
2021-02-01 22:52:02 -0600

    sync nodemailer and env module

--- mailer.js
+++ mailer.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer')
-
+var dotenv = require("dotenv")
+dotenv.config()
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth:{
-    user:process.env.email, 
-    pass: process.env.password  
+    user: process.env.EMAIL, 
+    pass: process.env.PASSWORD 
   }
 })
 
